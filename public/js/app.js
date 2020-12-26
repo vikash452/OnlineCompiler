4dc3301dc9115febf8bd2187d7a82739a6d24b83
Vikash <vikashp0901@gmail.com>
2020-12-26 22:53:29 +0530

    compiler implemented

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,50 @@
 document.getElementById('compileCode').addEventListener('click',()=>{
     console.log("clicked")
+
+    const codeWritten=document.getElementById('code').value;
+    const language=document.getElementById('cars').value;
+    const inputGiven=document.getElementById('input').value;
+
+    const data={
+        codeWritten,
+        language:"cpp17",
+        inputGiven,
+    }
+
+    fetch('/compileCode',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res=>res.json())
+    .then((res2)=>{
+        /*
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://api.jdoodle.com/v1/execute";
+
+        fetch(proxy+url,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                script: codeWritten,
+                language: "cpp17",
+                versionIndex: "0",
+                stdin: inputGiven,
+                clientId:data.id,
+                clientSecret:data.secret,
+            })
+        })
+        .then(res=>res.json())
+        .then((res2)=>{
+            console.log(res2);
+        })*/
+        console.log(res2.output)
+    })
+ 
 })
 
 document.getElementById('cars').addEventListener('change',()=>{
--- server.js
+++ server.js
@@ -1,11 +1,57 @@
 const express=require('express');
 const app=express();
+const fetch=require('node-fetch')
+const bodyParser=require('body-parser');
+require('dotenv').config();
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/index.html')
 })
 
-app.use(express.static('public'))
+app.post('/compileCode',(req,res)=>{
+    // console.log(req.body.codeWritten)
+    // console.log(req.body.language)
+    // console.log(req.body.inputGiven)
+
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://api.jdoodle.com/v1/execute";
+
+    fetch(proxy+url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body:JSON.stringify({
+            script: req.body.codeWritten,
+            language: req.body.language,
+            versionIndex: "0",
+            stdin: req.body.inputGiven,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+        })
+    })
+    .then(t=>t.json())
+    .then((data)=>{
+        res.json(data)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
+app.get('/getCredentials',(req,res)=>{
+    const credentials={
+        id:process.env.CLIENT_ID,
+        secret:process.env.CLIENT_SECRET
+    }
+    console.log(req.body)
+    res.json(credentials);
+})
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
